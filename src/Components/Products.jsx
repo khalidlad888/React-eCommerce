89423581a804031b4138248cc8c5eda9ae8e96b2
Khalid <khalidlad888@gmail.com>
2023-10-21 13:05:21 +0530

    Minor bug 2

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -90,91 +90,94 @@ const Products = () => {
         <div>
           {/* if loading is true show loading text */}
           {loading === true ? <h2>...Loading</h2> :
-            <div className='btn-div'>
-              <button onClick={resetSort}>Reset Sort</button>
-              <button onClick={sortByPrice}>Sort by Price</button>
-            </div>}
-
-          <div className="product-container">
-            {products.map((product) => (
-              <div key={product.id} className="product-card">
-                {/* shoeing edit form if edit button is clicked */}
-                {editedProduct && editedProduct.id === product.id ? (
-                  <div className="edit-form">
-                    Title -
-                    <input
-                      type="text"
-                      name="title"
-                      value={editedProduct.title}
-                      onChange={handleInputChange}
-                    />
-                    <br />
-                    Description -
-                    <input
-                      type="text"
-                      name="description"
-                      value={editedProduct.description}
-                      onChange={handleInputChange}
-                    />
-                    <br />
-                    Rating -
-                    <input
-                      type="number"
-                      name="rating"
-                      value={editedProduct.rating}
-                      onChange={handleInputChange}
-                    />
-                    <br />
-                    Price -
-                    <input
-                      type="number"
-                      name="price"
-                      value={editedProduct.price}
-                      onChange={handleInputChange}
-                    />
-                    <br />
-                    Image URL -
-                    <input
-                      type="text"
-                      name="image"
-                      value={editedProduct.image}
-                      onChange={handleInputChange}
-                    />
-                    <br />
-                    Category -
-                    <input
-                      type="text"
-                      name="category"
-                      value={editedProduct.category}
-                      onChange={handleInputChange}
-                    />
-                    <br />
-                    <button onClick={handleSave}>Save</button>
-                  </div>
-
-                ) : (
-                  <>
-                    <Link to={`/products/${product.id}`} className="product-link">
-                      <img src={product.image} alt={product.title} className="product-image" />
-                      <p className="product-name">{product.title}</p>
-                    </Link>
-                    <p>{product.description}</p>
-                    <p className="product-price">{product.price}</p>
-                    <div className="product-rating">
-                      <span>Rating:</span>
-                      {renderStars(product.rating)}
-                    </div>
-                    <p className="product-category">Category: {product.category}</p>
-                    <div className='addCart-div'>
-                      <FaEdit onClick={() => handleEdit(product)} className="edit-icon" />
-                      <FaTrash onClick={() => handleDelete(product.id)} className="delete-icon" />
-                      <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
-                    </div>
-                  </>
-                )}
+            <div>
+              <div className='btn-div'>
+                <button onClick={resetSort}>Reset Sort</button>
+                <button onClick={sortByPrice}>Sort by Price</button>
               </div>
-            ))}
-          </div>
+
+              <div className="product-container">
+                {products.map((product) => (
+                  <div key={product.id} className="product-card">
+                    {/* shoeing edit form if edit button is clicked */}
+                    {editedProduct && editedProduct.id === product.id ? (
+                      <div className="edit-form">
+                        Title -
+                        <input
+                          type="text"
+                          name="title"
+                          value={editedProduct.title}
+                          onChange={handleInputChange}
+                        />
+                        <br />
+                        Description -
+                        <input
+                          type="text"
+                          name="description"
+                          value={editedProduct.description}
+                          onChange={handleInputChange}
+                        />
+                        <br />
+                        Rating -
+                        <input
+                          type="number"
+                          name="rating"
+                          value={editedProduct.rating}
+                          onChange={handleInputChange}
+                        />
+                        <br />
+                        Price -
+                        <input
+                          type="number"
+                          name="price"
+                          value={editedProduct.price}
+                          onChange={handleInputChange}
+                        />
+                        <br />
+                        Image URL -
+                        <input
+                          type="text"
+                          name="image"
+                          value={editedProduct.image}
+                          onChange={handleInputChange}
+                        />
+                        <br />
+                        Category -
+                        <input
+                          type="text"
+                          name="category"
+                          value={editedProduct.category}
+                          onChange={handleInputChange}
+                        />
+                        <br />
+                        <button onClick={handleSave}>Save</button>
+                      </div>
+
+                    ) : (
+                      <>
+                        <Link to={`/products/${product.id}`} className="product-link">
+                          <img src={product.image} alt={product.title} className="product-image" />
+                          <p className="product-name">{product.title}</p>
+                        </Link>
+                        <p>{product.description}</p>
+                        <p className="product-price">{product.price}</p>
+                        <div className="product-rating">
+                          <span>Rating:</span>
+                          {renderStars(product.rating)}
+                        </div>
+                        <p className="product-category">Category: {product.category}</p>
+                        <div className='addCart-div'>
+                          <FaEdit onClick={() => handleEdit(product)} className="edit-icon" />
+                          <FaTrash onClick={() => handleDelete(product.id)} className="delete-icon" />
+                          <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
+                        </div>
+                      </>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+          }
         </div>
       }
     </div>
